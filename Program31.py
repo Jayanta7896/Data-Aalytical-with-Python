c343f08e947550038a44a06965423ae3392cbd67
Jayanta Karmakar <jeetukarmakar4@gmail.com>
2024-01-31 16:17:14 +0530

    Create Program31.py
    Python program to find smallest number in a list

---
+++ Program31.py
@@ -0,0 +1,9 @@
+#Python program to find smallest number in a list
+# Define a list
+my_list = [5, 2, 8, 1, 9, 3]
+
+# Find the smallest number using the min() function
+smallest_number = min(my_list)
+
+# Print the result
+print(f"The smallest number in the list is: {smallest_number}")
