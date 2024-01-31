f93cd46acdfb2380206fc4095ebaae9f3a3086e1
Jayanta Karmakar <jeetukarmakar4@gmail.com>
2024-01-31 16:10:13 +0530

    Create Program26.py
    Python | Ways to check if element exists in list

---
+++ Program26.py
@@ -0,0 +1,8 @@
+#Python | Ways to check if element exists in list
+my_list = [1, 2, 3, 4, 5]
+element_to_check = 3
+
+if element_to_check in my_list:
+    print(f"{element_to_check} exists in the list.")
+else:
+    print(f"{element_to_check} does not exist in the list.")
