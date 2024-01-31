2d7c131fb03b5931808e0e2626e829cddbc26839
Jayanta Karmakar <jeetukarmakar4@gmail.com>
2024-01-31 16:14:25 +0530

    Create Program29.py
    Python program to find sum of elements in list

---
+++ Program29.py
@@ -0,0 +1,9 @@
+#Python program to find sum of elements in list
+# Define a list
+my_list = [1, 2, 3, 4, 5]
+
+# Calculate the sum of elements using the sum() function
+sum_of_elements = sum(my_list)
+
+# Print the result
+print(f"The sum of elements in the list is: {sum_of_elements}")
