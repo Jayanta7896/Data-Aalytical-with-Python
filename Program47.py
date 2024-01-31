f49eed15ac9032d80fb6034dc4c8baee47bca290
Jayanta Karmakar <jeetukarmakar4@gmail.com>
2024-01-31 16:50:26 +0530

    Create Program47.py
    Python | Count occurrences of an element in a list

---
+++ Program47.py
@@ -0,0 +1,7 @@
+#Python | Count occurrences of an element in a list
+my_list = [1, 2, 3, 4, 1, 2, 1, 5]
+
+# Count occurrences of the element '1'
+count_of_ones = my_list.count(1)
+
+print(f"The element '1' occurs {count_of_ones} times in the list.")
