7914947444d0e54b5e150b9dd8301146a28eb977
Jayanta Karmakar <jeetukarmakar4@gmail.com>
2024-01-31 16:18:15 +0530

    Create Program32.py
    Python program to find largest number in a list

---
+++ Program32.py
@@ -0,0 +1,9 @@
+#Python program to find largest number in a list
+# Define a list
+my_list = [5, 2, 8, 1, 9, 3]
+
+# Find the largest number using the max() function
+largest_number = max(my_list)
+
+# Print the result
+print(f"The largest number in the list is: {largest_number}")
