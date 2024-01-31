13cb1e26267f21d78246c92ca7d2c834fbb05954
Jayanta Karmakar <jeetukarmakar4@gmail.com>
2024-01-31 16:12:09 +0530

    Create Program27.py
    Different ways to clear a list in Python

---
+++ Program27.py
@@ -0,0 +1,10 @@
+#Different ways to clear a list in Python
+#Using clear() method:
+my_list = [1, 2, 3, 4, 5]
+my_list.clear()
+print("List after clearing:", my_list)
+#Reassigning an empty list:
+my_list = [1, 2, 3, 4, 5]
+my_list = []
+print("List after clearing:", my_list)
+
